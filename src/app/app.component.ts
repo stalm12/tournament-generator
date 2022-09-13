09fbdece84cf1180f1df07ec3d82c0b5a37d71bf
Marius Stalder (U118853) <marius.stalder@mobi.ch>
2022-09-13 15:22:24 +0200

    add some calculations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Participant} from "./types/participant";
+import {isElementScrolledOutsideView} from "@angular/cdk/overlay/position/scroll-clip";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private STORAGE_KEY = "participants"
+
   title = 'tournament-generator';
   inputTN: string = "";
   participants: Participant[] = [];
+
+
+  ngOnInit(): void {
+    let storedParticipants = localStorage.getItem(this.STORAGE_KEY);
+    if(storedParticipants != null) {
+      this.participants = JSON.parse(storedParticipants);
+    }
+  }
 
   import() {
     let lines = this.inputTN.split("\n");
     let allTn = lines.map(l => this.createTN(l.split("\t")))
     this.participants = allTn;
+    this.saveToLocalStore();
     console.log(allTn);
   }
 
@@ -26,5 +39,24 @@ export class AppComponent {
       lastName,
       grade
     }
+  }
+
+  calculateNumberOfTnInPreRound(tn: number) {
+    let sizeOfPreRound = this.calculateTournamentSize(tn);
+    let sizeOfFirstRound = sizeOfPreRound / 2;
+    return (tn - sizeOfFirstRound) * 2;
+  }
+
+
+  calculateTournamentSize(tn: number) {
+    let size = 0
+    for(let i = 1; size < tn; i++){
+      size = Math.pow(2, i);
+    }
+    return size;
+  }
+
+  saveToLocalStore() {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.participants));
   }
 }
